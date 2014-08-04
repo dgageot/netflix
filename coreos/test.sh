7da0a2784f21bf33da71e9de5db8fc68b5248a2f
David Gageot <david@gageot.net>
2014-08-04 15:36:31 +0200

    test

---
+++ coreos/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl http://curlmyip.com/
+#curl --proxy 'http://didier:thatsdockerpower!@IP:80' http://curlmyip.com/
+curl --proxy 'http://didier:thatsdockerpower!@130.211.65.5:80' http://curlmyip.com/
