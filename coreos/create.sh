42668ae408e1053700481a22ef367365c1f9bef6
David Gageot <david@gageot.net>
2014-08-05 19:21:14 +0200

    us central

--- coreos/create.sh
+++ coreos/create.sh
@@ -4,7 +4,7 @@ gcutil \
 	--project="code-story-blog" \
 	addinstance \
 	--image="projects/coreos-cloud/global/images/coreos-stable-367-1-0-v20140724" \
-	--zone="europe-west1-b" \
+	--zone="us-central1-a" \
 	--machine_type="g1-small" \
 	--tags="http-server" \
 	--auto_delete_boot_disk \
