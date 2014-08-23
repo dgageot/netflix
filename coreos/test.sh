6275d3d76d0acc86340b8af0c55ed16f5c65df0a
David Gageot <david@gageot.net>
2014-08-23 16:53:55 +0200

    better test script

--- coreos/test.sh
+++ coreos/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+IP=$(gcutil listinstances --columns=name,external-ip --format=csv | grep netflix | cut -f2 -d',')
+
 curl http://curlmyip.com/
-#curl --proxy 'http://didier:thatsdockerpower!@IP:80' http://curlmyip.com/
-curl --proxy 'http://didier:thatsdockerpower!@130.211.65.5:80' http://curlmyip.com/
+curl --proxy "http://didier:thatsdockerpower!@${IP}:80" http://curlmyip.com/
